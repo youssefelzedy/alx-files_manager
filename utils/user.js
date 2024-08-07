23a81038a18621e067c60f690b85cf4a9e45ccaf
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-08-07 21:20:11 +0300

    create a new user

---
+++ utils/user.js
@@ -0,0 +1,44 @@
+import redisClient from './redis';
+import dbClient from './db';
+
+/**
+ * Module with user utilities
+ */
+const userUtils = {
+    /**
+     * Gets a user id and key of redis from request
+     * @request {request_object} express request obj
+     * @return {object} object containing userId and
+     * redis key for token
+     */
+    async getUserIdAndKey(request) {
+        const obj = {
+            userId: null,
+            key: null,
+        };
+
+        const token = request.header('X-Token');
+
+        if (!token) {
+            return obj;
+        }
+
+        obj.key = `auth_${token}`;
+        obj.userId = await redisClient.get(obj.key);
+
+        return obj;
+    },
+
+    /**
+     * Gets a user from database
+     * @query {object} query expression for finding
+     * user
+     * @return {object} user document object
+     */
+    async getUser(query) {
+        const user = await dbClient.usersCollection.findOne(query);
+        return user;
+    },
+};
+
+export default userUtils;
