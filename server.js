043fc69202178ce31b1e88249fe6c8d42c452105
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-08-07 20:32:35 +0300

    first router api

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import controllerRouting from './routes/index';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+controllerRouting(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
